4d803897e4c876866bbad8f8a2cfa6281369adee
Daniel Fernandes <danielteotonio@sapo.pt>
2020-03-05 10:42:17 +0000

    Add user input and formulas

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -6,7 +6,29 @@ namespace Cilindro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            //Variables
+            const float pi = 3.1415926f;
+            float radius, height = 0;
+            float area, volume;
+
+            //Ask For Input
+            Console.WriteLine("Please introduce height and radius!");
+            
+            //Parse Input to float
+            if(!float.TryParse(Console.ReadLine(), out radius) 
+            || !float.TryParse(Console.ReadLine(), out height)){
+                Console.WriteLine("Isso n e um float.");
+                Environment.Exit(0);
+            }
+
+            //Volume
+            volume = pi * radius * radius * height;
+
+            //Area
+            area = 2 * pi * radius * (radius + height);
+
+            Console.WriteLine("Volume: " + volume + " Area: " + area);
         }
     }
 }
