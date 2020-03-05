477e6b38414448afce215a28ed1f57f864c31122
Daniel Fernandes <danielteotonio@sapo.pt>
2020-03-05 09:32:56 +0000

    Add Operators

---
+++ IncDec/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a =2, b = 4;
+
+            int c = a++ + ++a;
+            
+            int d = c + --b + a;
+            Console.WriteLine("a: " + a);
+            Console.WriteLine("b: " + b);
+            Console.WriteLine("c: " + c);
+            Console.WriteLine("d: " + d);
+
+        }
+    }
+}
